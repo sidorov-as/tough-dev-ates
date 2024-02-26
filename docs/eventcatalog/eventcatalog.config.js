7d65beb25c31957a236c058e9e8600a4a23a1a84
sidorov-as <ereon.dev@gmail.com>
2024-02-26 10:59:41 +0300

    Add eventcatalog

---
+++ docs/eventcatalog/eventcatalog.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: 'aTES',
+  tagline: 'Awesome Task Exchange System',
+  organizationName: 'UberPopug Inc',
+  projectName: 'aTES',
+  basePath: '/tough-dev-ates',
+  trailingSlash: true,
+  primaryCTA: {
+    label: 'Explore Events',
+    href: '/events'
+  },
+  logo: {
+    alt: 'EventCatalog Logo',
+    src: 'logo.svg',
+  },
+  headerLinks: [
+    { label: 'Events', href: '/events'},
+    { label: 'Services', href: '/services' },
+    { label: 'Domains', href: '/domains'},
+    { label: 'Visualiser', href: '/visualiser' },
+    { label: '3D Node Graph', href: '/overview' },
+  ],
+  footerLinks: [
+    { label: 'Events', href: '/events' },
+    { label: 'Services', href: '/services' },
+    { label: 'Visualiser', href: '/visualiser' },
+    { label: '3D Node Graph', href: '/overview' },
+    { label: 'GitHub', href: 'https://github.com/boyney123/eventcatalog-demo/edit/master' }
+  ]
+}
---
+++ docs/eventcatalog/services/Payment Service/index.md
@@ -0,0 +1,13 @@
+---
+name: Payment Service
+summary: |
+  Event based application that integrates with Stripe.
+---
+
+The payment service is our own internal payment service that listens to events from stripe and handles them within our own domain. 
+
+We use Stripe to handle services and use this Payment service to enrich events for internal use.
+
+<OpenAPI />
+
+<NodeGraph />
